71a4557e12cd9d6a840b07ee64afaa312f579a8e
FreshMag <pescomagnani@gmail.com>
2024-12-24 11:24:18 +0100

    build: added publishOnCentral block

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.danilopianini.gradle.mavencentral.DocStyle
+
 plugins {
     alias(libs.plugins.ktMpp.mavenPublish)
 }
@@ -24,6 +26,36 @@ kotlin {
         getByName("jsMain") {
             dependencies {
                 // put your JVM test dependencies here
+            }
+        }
+    }
+}
+
+publishOnCentral {
+    val githubSlug = "FreshMag/testo"
+    projectUrl = "https://github.com/$githubSlug"
+    projectDescription = "Simple Kotlin Multiplatform test project"
+    licenseName = "Apache License 2.0"
+    projectLongName = "Testo Kotlin Multiplatform test project"
+    licenseUrl = "https://opensource.org/license/Apache-2.0/"
+    docStyle = DocStyle.HTML
+    publishing {
+        publications {
+            withType<MavenPublication>().configureEach {
+                if ("OSSRH" !in name) {
+                    artifact(tasks.javadocJar)
+                }
+                scmConnection = "git:git@github.com:$githubSlug"
+                projectUrl = "https://github.com/$githubSlug"
+                pom {
+                    developers {
+                        developer {
+                            name = "Francesco Magnani"
+                            email = "magnani.franci2000@gmail.com"
+                            url = "https://github.com/FreshMag"
+                        }
+                    }
+                }
             }
         }
     }
