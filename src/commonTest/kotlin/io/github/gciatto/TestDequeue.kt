5b9ae4c90f8639ae626390794ea70ab7e9fe7a4f
FreshMag <pescomagnani@gmail.com>
2025-01-07 18:36:18 +0100

    test: added failing test

--- src/commonTest/kotlin/io/github/gciatto/TestDequeue.kt
+++ src/commonTest/kotlin/io/github/gciatto/TestDequeue.kt
@@ -23,6 +23,10 @@ class TestDequeue :
             dequeOf(1, 2, 3).also { it.add(4) } shouldBe dequeOf(1, 2, 3, 4)
         }
 
+        "A failing test" {
+            true shouldBe false
+        }
+
 //    @Test
 //    fun testPrepend() {
 //        assertEquals(
