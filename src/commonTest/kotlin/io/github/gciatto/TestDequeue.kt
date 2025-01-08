e7e7068b658d6a053a18978c59e8ec9afa0a5446
FreshMag <pescomagnani@gmail.com>
2025-01-08 10:29:39 +0100

    style: removed style violation

--- src/commonTest/kotlin/io/github/gciatto/TestDequeue.kt
+++ src/commonTest/kotlin/io/github/gciatto/TestDequeue.kt
@@ -5,8 +5,6 @@ import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 
 class TestDequeue :
-
-
     StringSpec({
 
         "A deque should be created from literals" {
