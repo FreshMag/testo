0c55aae2a2675b7db2adcb02e374f53e18f89ee5
FreshMag <pescomagnani@gmail.com>
2025-01-09 21:06:48 +0100

    test: added more tests for yaml

--- src/commonTest/kotlin/io/github/gciatto/TestYaml.kt
+++ src/commonTest/kotlin/io/github/gciatto/TestYaml.kt
@@ -47,4 +47,48 @@ class TestYaml : StringSpec({
         person.age shouldBe 30
         person.city shouldBe null
     }
+
+    "YAML with arrays" {
+        @Serializable
+        data class Person(
+            val name: String,
+            val telephones: List<String>,
+        )
+
+        val yaml =
+            """
+            name: John Doe
+            telephones:
+              - 123456
+              - 789012
+            """.trimIndent()
+
+        val person: Person = Yaml.decodeFromString(Person.serializer(), yaml)
+        person.name shouldBe "John Doe"
+        person.telephones shouldBe listOf("123456", "789012")
+    }
+
+    "YAML arrays with single object" {
+        val yaml =
+            """
+            name: John Doe
+            telephones: "123456"
+            """.trimIndent()
+
+        val person: Map<String?, Any?> = Yaml.decodeMapFromString(yaml)
+        person["name"] shouldBe "John Doe"
+        person["telephones"] shouldBe "123456"
+
+        val yaml2 =
+            """
+            name: John Doe
+            telephones: 
+                - "123456"
+                - "789012"
+            """.trimIndent()
+        val person2: Map<String?, Any?> = Yaml.decodeMapFromString(yaml2)
+        person2["name"] shouldBe "John Doe"
+        println(person2["telephones"])
+        person2["telephones"] shouldBe listOf("123456", "789012")
+    }
 })
